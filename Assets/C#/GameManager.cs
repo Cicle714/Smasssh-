194e99c0efe28ac82b74fe6a5e3c03ebf1b3b8bc
S <>
2025-11-19 19:45:19 +0900

    仮のタイトル追加

--- Assets/C#/GameManager.cs
+++ Assets/C#/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,9 +12,17 @@ public class GameManager : MonoBehaviour
     private Enemy[] Enemys;
     private int EnemyNum = 0;
     public int Point;
+    [SerializeField]
+    private GameObject ClearText;
+
+    [SerializeField]
+    private Image BlackOut;
+    private float BlackOutCount;
+    private float BlackOutTime = 2;
 
     void Start()
     {
+        StartCoroutine(_BlackOut());
     }
 
 
@@ -39,7 +49,26 @@ public class GameManager : MonoBehaviour
 
         if(Point >= Enemys.Length)
         {
+            ClearText.SetActive(true);
+            BlackOutCount += Time.deltaTime;
+            if(BlackOutCount >= BlackOutTime)
+            {
+                BlackOut.color += new Color(0, 0, 0, 1 * Time.deltaTime / 2);
+                if(BlackOut.color.a >= 1)
+                {
+                    SceneManager.LoadScene("Title");
+                }
+            }
+        }
 
+    }
+
+    IEnumerator _BlackOut()
+    {
+        while(BlackOut.color.a > 0)
+        {
+            BlackOut.color -= new Color(0, 0, 0, 1 * Time.deltaTime / 2);
+            yield return null;
         }
 
     }
